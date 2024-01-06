39ce9ae2b495284b04c5c9f6a374206f3985320e
samsunny16 <samsunny0716@gmail.com>
2024-01-07 00:43:50 +0530

    initial changes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const initialitems = [
+  {id:1,description:"Passports",quantity:2,packed:false},
+  {id:2,description:"socks",quantity:6,packed:false}
+]
+export default function App (){
+  return <div className="app">
+    <Logo/>
+    <Form/>
+    <PackingList/>
+    <Stats/>
+  </div>
 }
 
-export default App
+function Logo(){
+  return <h1> FAR AWAY</h1>
+
+}
+function Form(){
+  return <div className="add-form">
+    <h3>what do you need for the trip?</h3>
+  </div>
+  
+}
+function PackingList(){
+  return <ul className="list">
+    {initialitems.map((item) =>(
+      <Item item = {item}/>
+    ) )}
+  </ul>
+  
+}
+function Stats(){
+  return <footer> stats to be </footer>
+  
+}
+function Item( {item} ){
+  return <li><span>{item.quantity}{" "}{item.description}</span><button>press</button></li>
+
+}
